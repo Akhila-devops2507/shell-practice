66106f0700f61bf5d1735b46ead62d6f8364fa12
akhila reddy <akhila@devops.com>
2025-09-24 21:28:37 +0530

    variables

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+#!/bin/bash
+
 number=$1
 
-if [ $number -less than 100]; then
-    echo "Given number $number less than 100"
-elif [ $number -eq 100 ]; then
+if [ "$number" -lt 100 ]; then
+    echo "Given number $number is less than 100"
+elif [ "$number" -eq 100 ]; then
     echo "Given number $number is equal to 100"
 else
     echo "Given number $number is greater than 100"
