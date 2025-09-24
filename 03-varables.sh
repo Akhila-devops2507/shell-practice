1cf9843db896f09e206963604521959a39052218
akhila reddy <akhila@devops.com>
2025-09-24 16:32:18 +0530

    some message

---
+++ 03-varables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=lucky
+Person2=venky
+
+echo "$Person1:: What is your name?"
+echo "$Person2:: My name is $Person2."
+echo "$Person1:: How are you?"
+echo "$Person2:: I am fine, thank you!"
