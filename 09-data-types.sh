e3e9a61a56fad19d68ad7cafe43fa0619d63c929
akhila reddy <akhila@devops.com>
2025-09-24 20:55:03 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -5,3 +5,8 @@ NUMBER2=20
 
 SUM=$((NUMBER1 + NUMBER2))
 echo "Sum is: $SUM"
+
+LEADERS=("modi" "yogi" "adityanath" "rajnath" "shah")
+echo "All leaders: ${LEADERS[@]}"
+echo "First leader: ${LEADERS[0]}"
+echo "Number of leaders: ${#LEADERS[@]}"
