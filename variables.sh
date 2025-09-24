3c28a93c7d03f43f41042a59961e463625b03708
akhila reddy <akhila@devops.com>
2025-09-24 18:44:43 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Welcome to india."
+echo "$PERSON2:: Thank you, Mr. $PERSON1."
+echo "$PERSON1:: How are you?"
+echo "$PERSON2:: I am fine, thank you!"
