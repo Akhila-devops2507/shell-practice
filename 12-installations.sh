8344a13aa3cba410fd51be2f6151c4b4691965df
akhila reddy <akhila@devops.com>
2025-09-25 11:56:22 +0530

    variables

--- 12-installations.sh
+++ 12-installations.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Error:: Please run this script as root user"
+    exit 1
 fi
 
 dnf install mysql -y
@@ -11,4 +12,5 @@ if [ $? -eq 0 ]; then
     echo "MySQL installed successfully"
 else
     echo "Error:: MySQL installation failed"
+    exit 1
 fi
