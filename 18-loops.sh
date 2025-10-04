9f6d1562b7fdd19cd1fc22242ba432bdd8f7aebd
akhila reddy <akhila@devops.com>
2025-10-04 17:09:02 +0530

    some message

--- 18-loops.sh
+++ 18-loops.sh
@@ -6,24 +6,24 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-logs_folder="/var/logs/shell-scripts"
-script_name=$( echo $0 | cut -d "." -f1 )
-log_file="$logs_folder/$script_name.log"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p $logs_folder
-echo "script started executed at: $(date)" | tee -a $log_file
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo "Error:: Please run this script with the root privileges"
-    exit 1
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "installing $2 ... $R failure $N" | tee -a $log_file
+        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "installing $2 ... $R success $N" | tee -a $log_file
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
@@ -31,7 +31,7 @@ VALIDATE(){
 
 for package in $@
 do
-   # check package is already installed or not
+    # check package is already installed or not
     dnf list installed $package &>>$LOG_FILE
 
     # if exit status is 0, already installed. -ne 0 need to install it
@@ -41,4 +41,5 @@ do
     else
         echo -e "$package already installed ... $Y SKIPPING $N"
     fi
-done    
+done
+
