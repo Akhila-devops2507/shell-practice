e5f6627e128cbc3c0dee8ada619127c4da7fc8df
akhila reddy <akhila@devops.com>
2025-10-07 11:56:18 +0530

    message

--- 21-script-1.sh
+++ 21-script-1.sh
@@ -3,6 +3,6 @@
 NAME=INDIA
 echo "My Country: $NAME"
 echo "PID of SCRITP-1: $$"
-#sh 21-script-2.sh
-source ./21-script-2.sh
+#sh 22-script-2.sh
+source ./22-script-2.sh
 echo "Back to script-1: $NAME"2
